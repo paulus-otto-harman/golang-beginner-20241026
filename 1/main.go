23a7ecbb57dd63849c1ba1b70fb08eccb6a68dee
Paulus Otto Harman <lonewolfffffff@gmail.com>
2024-10-26 15:17:19 +0700

    AppContainer

--- 1/main.go
+++ 1/main.go
@@ -8,26 +8,28 @@ import (
 )
 
 func main() {
+	const SessionTimeout = 2
+
 	wg := sync.WaitGroup{}
 
 	wg.Add(1)
-	go resettable(&wg)
+	go AppContainer(&wg, SessionTimeout)
 
 	wg.Wait()
 }
 
-func resettable(wg *sync.WaitGroup) {
+func AppContainer(wg *sync.WaitGroup, timeout int) {
 	defer wg.Done()
 	ticker := time.NewTicker(time.Second)
-
+	sessionLifetime := time.Duration(timeout) * time.Second
 	for {
-		hasTimeout(2, ticker)
+		hasTimeout(sessionLifetime, ticker)
 	}
 
 }
 
-func hasTimeout(duration int, ticker *time.Ticker) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(duration)*time.Second)
+func hasTimeout(duration time.Duration, ticker *time.Ticker) {
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
 	defer cancel()
 	ticks(ticker, ctx)
 }
@@ -42,6 +44,5 @@ func ticks(ticker *time.Ticker, ctx context.Context) {
 			fmt.Println("timeout")
 			return
 		}
-
 	}
 }
